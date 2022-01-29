d90ba0bbf0559a584fee64cb0ead1e745b8d1dd8
dineshpatilcloud <66240036+dineshpatilcloud@users.noreply.github.com>
2022-01-29 15:11:42 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -47,7 +47,7 @@ variable "dns_prefix" {
 }
 variable "cluster_version" {
   type        = string
-  default     = "1.20.9"
+  default     = "1.21.2"
   description = "Kubernetes version to use for the AKS cluster"
 }
 variable "network_resource_group_name" {
